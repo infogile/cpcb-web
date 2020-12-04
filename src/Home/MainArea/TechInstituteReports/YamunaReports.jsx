fd2abbbcc12a841b10f81795fd83693370170bb6
Pragati Rana <shalu.pragati11@gmail.com>
2020-12-05 03:38:40 +0530

    first

--- src/Home/MainArea/TechInstituteReports/YamunaReports.jsx
+++ src/Home/MainArea/TechInstituteReports/YamunaReports.jsx
@@ -9,7 +9,7 @@ import "../../../assets/css/tech.css"
 
 export const YamunaReports =({ title })=>{
     return (
-        <>
+        <div>
         <header>
             <div class="container">
                 <h2 class="h1">
@@ -68,53 +68,56 @@ export const YamunaReports =({ title })=>{
             </div>
         </div>
         <div class="container">
-            <div class="rectangle2">
-                <div class="line" style={{ top: "340px", left: "45px" }}/>
-            </div>
             <div class="rectangle">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-6 col-sm-6">
-                            <div class="col-md-8 col-sm-7 text-align-right">
-                                <span class="texts" style={{ left: "210px", width: "85px" }}>Total Inspection</span>
-                                <span class="texts" style={{ left: "375px", width: "85px" }}>Inspection Pending</span>
-                                <span class="texts" style={{ left: "530px", width: "98px" }}>Field Report Submitted</span>
-                                <span class="texts" style={{ left: "650px", width: "148px" }}>Inspection Report Submitted</span>
-                                <span class="texts" style={{ left: "815px", width: "151px" }}>Report Submitted Within 10 Days</span>
-                                <span class="texts" style={{ left: "990px", width: "139px" }}>Submitted More Than 10 Days</span>
-                            </div>
-                        </div>
+            </div>
+            <div class="rectangle2">
+                <div>
+                    <table>
+                        <tr>
+                            <td><span class="texts">Total Inspection</span><p class="line"></p></td>
+                            <td><span class="texts">Inspection Pending</span><p class="line"></p></td>
+                            <td><span class="texts">Field Report Submitted</span><p class="line"></p></td>
+                            <td><span class="texts">Inspection Report Submitted</span><p class="line"></p></td>
+                            <td><span class="texts">Report Submitted Within 10 Days</span><p class="line"></p></td>
+                            <td><span class="texts">Submitted More Than 10 Days</span><p class="line"></p></td>
+                        
+                        </tr>
+                    </table>
+                </div>
+                <div class="p1">
+                    <table>
+                        <tr ><td>CPRI</td></tr>
+                        <tr ><td>DTU</td></tr>
+                        <tr ><td>IIT Delhi</td></tr>
+                        <tr ><td>IIT Roorkee</td></tr>
+                        <tr ><td>CPRI</td></tr>
+                        <tr ><td>NEERI</td></tr>
+                        <tr ><td>JMI</td></tr>
+                    </table>
+                </div>
+                <div class="row">
+                    <div class="rectangle3" style={{ left: "210px" }}>
+                        <p class="n">3576</p>
                     </div>
-                </div>  
+                    <div class="rectangle3" style={{ left: "375px" }}>
+                        <p class="n">123</p>
+                    </div>
+                    <div class="rectangle3" style={{ left: "530px" }}>
+                        <p class="n">3445</p>
+                    </div>
+                    <div class="rectangle3" style={{ left: "670px" }}>
+                        <p class="n">2222</p>
+                    </div>
+                    <div class="rectangle3" style={{ left: "815px" }}>
+                        <p class="n">73737</p>
+                    </div>
+                    <div class="rectangle3" style={{ left: "990px" }}>
+                        <p class="n">3333</p>
+                    </div>
+                </div>
             </div>
-
-
-            <div class="rectangle3" style={{ left: "503px" }}>
-                <p class="n">3576</p>
-                <div class="line"/>
-            </div>
-            <div class="rectangle3" style={{ left: "666px" }}>
-                <p class="n">123</p>
-                <div class="line"/>
-            </div>
-            <div class="rectangle3" style={{ left: "820px" }}>
-                <p class="n">3445</p>
-                <div class="line"/>
-            </div>
-            <div class="rectangle3" style={{ left: "971px" }}>
-                <p class="n">2222</p>
-                <div class="line"/>
-            </div>
-            <div class="rectangle3" style={{ left: "1138px" }}>
-                <p class="n">73737</p>
-                <div class="line"/>
-            </div>
-            <div class="rectangle3" style={{ left: "1306px" }}>
-                <p class="n">3333</p>
-            </div>
-    
         </div>
-        </>
+    </div>
     );
 };
 
