5c0d6140f551d2125eff4f1ae98a8c24609b49b2
Pragati Rana <shalu.pragati11@gmail.com>
2020-12-07 18:53:54 +0530

    first

--- src/Home/MainArea/TechInstituteReports/StateWise.jsx
+++ src/Home/MainArea/TechInstituteReports/StateWise.jsx
@@ -252,86 +252,64 @@ export const StateWise =({ title })=>{
             </header>
             <body>
                 <div>
-                    <article class="sc-fznMAR iaBNQP">
-                        <div class="fVgyOA">
-                            <div class="food">
-                                <div class="f1"> 
-                                    <p class="style">Food and Beverages</p><div class="rect"><p>Export</p></div>
-                                </div>
-                            </div>
-                            <div class="tab">
-                                <table id="state">
-                                    <tr>
-                                        <th>S. NO.</th>
-                                        <th>UNIT CODE</th>
-                                        <th>UNIT NAME</th>
-                                        <th>REGION</th>
-                                        <th>SECTOR</th>
-                                        <th>INSPECTION DATE</th>
-                                        <th>FIELD REPORT</th>
-                                        <th>FINAL REPORT</th>
-                                    </tr>
-                                    <tr>
-                                        <td>Alfreds Futterkiste</td>
-                                        <td>Maria Anders</td>
-                                        <td>Germany</td>
-                                        <td>abc</td>
-                                        <td>segsedgf</td>
-                                        <td></td>
-                                        <td><a href="">View</a></td>
-                                        <td><a href="">View</a></td>
-                                    </tr>
-                                    <tr>
-                                        <td>Berglunds snabbköp</td>
-                                        <td>Christina Berglund</td>
-                                        <td>Sweden</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Centro comercial Moctezuma</td>
-                                        <td>Francisco Chang</td>
-                                        <td>Mexico</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Ernst Handel</td>
-                                        <td>Roland Mendel</td>
-                                        <td>Austria</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Island Trading</td>
-                                        <td>Helen Bennett</td>
-                                        <td>UK</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Königlich Essen</td>
-                                        <td>Philip Cramer</td>
-                                        <td>Germany</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Laughing Bacchus Winecellars</td>
-                                        <td>Yoshi Tannamuri</td>
-                                        <td>Canada</td>
-                                        </tr>
-                                    <tr>
-                                        <td>Magazzini Alimentari Riuniti</td>
-                                        <td>Giovanni Rovelli</td>
-                                        <td>Italy</td>
-                                    </tr>
-                                    <tr>
-                                        <td>North/South</td>
-                                        <td>Simon Crowther</td>
-                                        <td>UK</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Paris spécialités</td>
-                                        <td>Marie Bertrand</td>
-                                        <td>France</td>
-                                    </tr>
-                                </table>
-                            </div>
-                        </div>
-                    </article>
                     <div class="a">
+                        <Collapsible trigger="Food and Beverages">
+                            <button class="export">Export</button>
+                            <table id="state">
+                                <tr>
+                                    <th>S. NO.</th>
+                                    <th>UNIT CODE</th>
+                                    <th>UNIT NAME</th>
+                                    <th>REGION</th>
+                                    <th>SECTOR</th>
+                                    <th>INSPECTION DATE</th>
+                                    <th>FIELD REPORT</th>
+                                    <th>FINAL REPORT</th>
+                                </tr>
+                                <tr>
+                                    <td>Alfreds Futterkiste</td>
+                                    <td>Maria Anders</td>
+                                    <td>Germany</td>
+                                    <td>abc</td>
+                                    <td>segsedgf</td>
+                                    <td></td>
+                                    <td><a href="">View</a></td>
+                                    <td><a href="">View</a></td>
+                                </tr>
+                                <tr>
+                                    <td>Berglunds snabbköp</td>
+                                    <td>Christina Berglund</td>
+                                    <td>Sweden</td>
+                                </tr>
+                                <tr>
+                                    <td>Centro comercial Moctezuma</td>
+                                    <td>Francisco Chang</td>
+                                    <td>Mexico</td>
+                                </tr>
+                                <tr>
+                                    <td>Ernst Handel</td>
+                                    <td>Roland Mendel</td>
+                                    <td>Austria</td>
+                                </tr>
+                                <tr>
+                                    <td>Island Trading</td>
+                                    <td>Helen Bennett</td>
+                                    <td>UK</td>
+                                </tr>
+                                <tr>
+                                    <td>Königlich Essen</td>
+                                    <td>Philip Cramer</td>
+                                    <td>Germany</td>
+                                    </tr>
+                                <tr>
+                                    <td>Laughing Bacchus Winecellars</td>
+                                    <td>Yoshi Tannamuri</td>
+                                    <td>Canada</td>
+                                </tr>
+                            </table>
+                        </Collapsible>                        
                         <Collapsible trigger="Tannery">
+                        <button class="export">Export</button>
                             <table id="state">
                                 <tr>
                                     <th>S. NO.</th>
@@ -386,6 +364,7 @@ export const StateWise =({ title })=>{
                             </table>
                         </Collapsible>
                         <Collapsible trigger="Sugar">
+                            <button class="export">Export</button>
                             <table id="state">
                                 <tr>
                                     <th>S. NO.</th>
@@ -440,6 +419,7 @@ export const StateWise =({ title })=>{
                             </table>
                         </Collapsible>
                         <Collapsible trigger="Pharmaceutical">
+                            <button class="export">Export</button>
                             <table id="state">
                                 <tr>
                                     <th>S. NO.</th>
@@ -494,6 +474,7 @@ export const StateWise =({ title })=>{
                             </table>
                         </Collapsible>
                         <Collapsible trigger="Food, Dairy and Beverages">
+                            <button class="export">Export</button>
                             <table id="state">
                                 <tr>
                                     <th>S. NO.</th>
