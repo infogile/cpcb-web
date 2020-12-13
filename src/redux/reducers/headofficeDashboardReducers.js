4e52a7c8bd58a67f061b87a70fe2d9cf424bda18
1496-rajeev <1496piku@gmail.com>
2020-12-13 14:49:14 +0530

    headoffice

---
+++ src/redux/reducers/headofficeDashboardReducers.js
@@ -0,0 +1,31 @@
+export function headofficeDashboardReducers(
+    state = {
+        data:[],
+        isloading: true,
+        },
+        action
+) {
+    const {data, type} = action;
+    switch (type) {
+        case "INIT_HEADOFFICE_DAHBOARD":
+            return {
+                ...state,
+                isLoading: true,
+                };
+        case "GET_HEADOFFICE_DASHBOARD":
+        return {
+            ...state,
+            data,
+            isLoading: false,
+            };
+        case "ERROR_HEADOFFICE_DASHBOARD":
+            return {
+                ...state,
+                isLoading: false,
+                };
+        default:
+            return {
+                ...state,
+            };
+        }         
+    }
---
+++ src/redux/services/headofficeDashboardService.js
@@ -0,0 +1,18 @@
+import {initHeadofficeDashboardAction, HeadofficeDashboardSuccess,HeadofficeDashboardError} from "../actions";
+import axios from "../../axios"
+
+export function getHeadofficedashboard(){
+    return (dispatch) => {
+        dispatch(initHeadofficeDashboardAction());
+        axios
+        .post('/inspection/count')
+        .then((res) => {
+            console.log(res.data)
+            dispatch({ ...HeadofficeDashboardSuccess(), data: dashboardArray });
+        })
+        .catch((err) => {
+            console.log(err)
+            dispatch(HeadofficeDashboardError());
+        });
+    };
+}
