b40b255203b7837392adba6df85ae4d5f0b930db
hirensavalias <hiren.savalias@gmail.com>
2020-12-18 03:52:46 +0530

    Update index.js

--- src/axios/index.js
+++ src/axios/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   // baseURL: window.location.protocol + "//localhost:9090/api",
-  baseURL: "http://139.59.73.88:9090/api",
+  baseURL: window.location.protocol + "//" + window.location.host + ":9090/api",
   headers: {
     authorization: sessionStorage.getItem("token")
       ? sessionStorage.getItem("token")
